85de538516aa442b25417e55c21b5c575c542a75
Tanim reja <tanim2reja@gmail.com>
2015-08-10 15:08:24 +0600

    .

--- app/src/main/java/com/jolpai/tafsir/adapter/RecyclerItemViewHolder.java
+++ app/src/main/java/com/jolpai/tafsir/adapter/RecyclerItemViewHolder.java
@@ -21,47 +21,29 @@ public class RecyclerItemViewHolder extends RecyclerView.ViewHolder {
    // private final TextView mItemTextView;
     Context context;
     private final R2L  mCustomLayout;
-    int i=0;
-private static Typeface tf ;
-
-
-    /*public RecyclerItemViewHolder(final View parent, TextView itemTextView,View r2l) {
-        super(parent);
-        mItemTextView = itemTextView;
-        context=parent.getContext();
-
-
-    }*/
+    private static Typeface tf ;
 
     public RecyclerItemViewHolder(final View parent, R2L r2l) {
         super(parent);
         mCustomLayout = r2l;
         context=parent.getContext();
-
-
     }
 
     public static RecyclerItemViewHolder newInstance(View parent) {
-        //TextView itemTextView = (TextView) parent.findViewById(R.id.itemTextView);
-        tf=Typeface.createFromAsset(parent.getContext().getAssets(), "fonts/TRADO.TTF");
-        final  R2L customLayout = (R2L) parent.findViewById(R.id.cl_R2L);
-
+        tf=Typeface.createFromAsset(parent.getContext().getAssets(),"fonts/TRADO.TTF");
+        R2L customLayout = (R2L) parent.findViewById(R.id.cl_R2L);
 
         return new RecyclerItemViewHolder(parent, customLayout);
     }
 
     public void setItemText(CharSequence text) {
-        final View normalView =View.inflate(context, R.layout.normal_view, null);
-        final TextView tv = (TextView)normalView.findViewById(R.id.textView);
-
-      //  tv.setText("tanim"+i);
+        View normalView =View.inflate(context, R.layout.normal_view, null);
+        TextView tv = (TextView)normalView.findViewById(R.id.textView);
         tv.setText(text);
         tv.setTypeface(tf);
         tv.setTextSize(35);
         tv.setTextColor(Color.DKGRAY);
         mCustomLayout.removeAllViews();
         mCustomLayout.addView(normalView);
-        i++;
     }
-
 }
